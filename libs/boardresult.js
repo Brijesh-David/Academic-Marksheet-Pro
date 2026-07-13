3bd8642a7fb39d0722a8455002afa2b50adf72ef
Brijesh-David <brijeshdavid.socials@gmail.com>
2026-07-13 18:39:20 +0530

    Add files via upload

---
+++ libs/boardresult.js
@@ -0,0 +1,3 @@
+// Placeholder for Board Internal (v3.0) mode logic.
+// When implemented, define generateBoardReports(action) where action is 'view' or 'download'.
+// window.generateBoardReports = (action) => { ... };
